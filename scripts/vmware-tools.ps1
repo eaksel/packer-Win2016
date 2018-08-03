82ced1f8e6a911675c493a6abbc2d30e10be8837
eaksel <emre.aksel@ymail.com>
2018-08-03 20:12:27 +0200

    Added config files for Core and Desktop edition; changed vmware-tools installation script; added cleanup

--- scripts/vmware-tools.ps1
+++ scripts/vmware-tools.ps1
@@ -1,30 +1,14 @@
-# Source : https://github.com/luciusbono/Packer-Windows10/blob/master/install-guest-tools.ps1
+$package = "VMware-tools-10.2.1-8267844-x86_64.exe"
+$url = "https://packages.vmware.com/tools/releases/10.2.1/windows/x64/$package"
 
+Write-Output "***** Downloading VMWare tools"
+Invoke-WebRequest $url -UseBasicParsing -OutFile "C:\Windows\Temp\$package"
 
-$isopath = "C:\Windows\Temp\windows.iso"
-
-# Mount the .iso, then build the path to the installer by getting the Driveletter attribute from Get-DiskImage piped into Get-Volume and adding a :\setup.exe
-# A separate variable is used for the parameters. There are cleaner ways of doing this. I chose the /qr MSI Installer flag because I personally hate silent installers
-# Even though our build is headless. 
-
-Write-Output "***** Mounting disk image at $isopath"
-Mount-DiskImage -ImagePath $isopath
-
-function vmware {
-
-$exe = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\setup64.exe')
+$exe = "C:\Windows\Temp\$package"
 $parameters = '/S /v "/qn REBOOT=R ADDLOCAL=ALL"'
 
+Write-Output "***** Installing VMWare Guest Tools"
 Start-Process $exe $parameters -Wait
 
-}
-
-Write-Output "***** Installing VMWare Guest Tools"
-vmware
-
-#Time to clean up - dismount the image and delete the original ISO
-
-Write-Output "***** Dismounting disk image $isopath"
-Dismount-DiskImage -ImagePath $isopath
 Write-Output "***** Deleting $isopath"
-Remove-Item $isopath
+Remove-Item $exe
